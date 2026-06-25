2ca808c83d8afd727867bf89b240af4f3b828431
Usman Yahaya Baba <91813795+uthmaniv@users.noreply.github.com>
2026-06-25 23:38:12 +0100

    add: mining to 04.sh after wallet creation so 05.sh has non-zero balance so CI passes

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,3 @@
-# Create a wallet named "builderswallet"
+# Create a wallet named "builderswallet" and fund it
 bitcoin-cli -regtest createwallet "builderswallet"
+bitcoin-cli -regtest generatetoaddress 101 "$(bitcoin-cli -regtest getnewaddress)"
